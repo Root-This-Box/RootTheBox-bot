36dd210aca2889d2f23d8142e00a4960e0e628f5
realyStupid <ther3al.re4ly.stupid@gmail.com>
2026-04-25 11:23:02 -0500

    changed other.py file

--- bot/cogs/others.py
+++ bot/cogs/others.py
@@ -13,10 +13,6 @@ class other_commands(commands.Cog):
         Credits go to: 
     wired8, for helping on creating the bot and the RootThisBox website!
     therealrealystupid, for creating the discord bot you see today alongside his hacking journey!
-                                       
-QUICK NOTE FROM therealrealystupid: im kinda broke and working on bot and trying to teach myself hacking is kinda hard to do lol
-    so if you have money to spare please concider sending me a small some of bitcoin to me! only if u want tho, not forcing u
-    `bc1q27el8z637ll8p4flamztqwwmdpj79may3m0krl`
                                 
 thanks for being a part of this community and keep on hacking!
         """)
